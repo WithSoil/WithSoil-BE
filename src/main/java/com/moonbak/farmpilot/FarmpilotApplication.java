e38d326298b7d641cddbf64c1943490b9839a765
Yugyeong Park <bonbonly7@naver.com>
2026-06-06 20:29:13 +0900

    chore: initial project

---
+++ src/main/java/com/moonbak/farmpilot/FarmpilotApplication.java
@@ -0,0 +1,13 @@
+package com.moonbak.farmpilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmpilotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmpilotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moonbak/farmpilot/FarmpilotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moonbak.farmpilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmpilotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
