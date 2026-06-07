fc49ed06b77b5a3031c5a5f8de5a8837a9235a7e
Yugyeong Park <bonbonly7@naver.com>
2026-06-07 21:26:26 +0900

    feat: 요청 및 예외 로깅 기능 추가

---
+++ src/main/java/com/moonbak/farmpilot/global/aop/ExceptionLoggingAspect.java
@@ -0,0 +1,50 @@
+package com.moonbak.farmpilot.global.aop;
+
+import com.moonbak.farmpilot.global.exeption.BaseException;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Aspect
+@Component
+public class ExceptionLoggingAspect {
+
+    @Pointcut("execution(* com.moonbak.farmpilot.api..service..*(..))")
+    private void applicationLayer() {}
+
+    // 메서드를 실행하다가 에러(Exception)가 터졌을 때만 작동
+    @AfterThrowing(
+            pointcut = "applicationLayer()",
+            throwing = "ex"
+    )
+    public void logException(JoinPoint joinPoint, Exception ex) {
+
+        String className = joinPoint.getSignature().getDeclaringType().getSimpleName();
+        String methodName = joinPoint.getSignature().getName();
+        Object[] args = joinPoint.getArgs();
+
+        if (ex instanceof BaseException baseEx) {
+
+            log.warn("[Exception] {}.{}() | args={} | {} - {}",
+                    className,
+                    methodName,
+                    args,
+                    baseEx.getClass().getSimpleName(),
+                    baseEx.getMessage()
+            );
+        } else {
+            log.error("[Exception] {}.{}() | args={} | {} - {}",
+                    className,
+                    methodName,
+                    args,
+                    ex.getClass().getSimpleName(),
+                    ex.getMessage(),
+                    ex
+            );
+        }
+    }
+}
