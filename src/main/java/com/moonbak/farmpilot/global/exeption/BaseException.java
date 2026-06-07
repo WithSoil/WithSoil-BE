8c466724e8d65d536ab4fa8089d7afef692069c5
Yugyeong Park <bonbonly7@naver.com>
2026-06-07 21:26:13 +0900

    feat: 전역 API 응답 및 예외 처리 구조 추가

---
+++ src/main/java/com/moonbak/farmpilot/global/exeption/BaseException.java
@@ -0,0 +1,24 @@
+package com.moonbak.farmpilot.global.exeption;
+
+import com.moonbak.farmpilot.global.response.ErrorStatus;
+import lombok.Getter;
+
+@Getter
+public class BaseException extends RuntimeException {
+
+    private final ErrorStatus errorStatus;
+
+    public BaseException(ErrorStatus errorStatus) {
+        super(errorStatus.getMessage());
+        this.errorStatus = errorStatus;
+    }
+
+    public BaseException(ErrorStatus errorStatus, Throwable cause) {
+        super(errorStatus.getMessage(), cause);
+        this.errorStatus = errorStatus;
+    }
+
+    public int getStatusCode() {
+        return this.errorStatus.getStatusCode();
+    }
+}
