fe48230c263f272dc14b9b305874110f64b0a1b7
thadus2 <seunghoon3753@gmail.com>
2026-06-21 15:25:11 +0900

    feat: 병해 진단 관련 파일 추가

--- src/main/java/com/outthedoor/withsoil/api/ai/controller/AiController.java
+++ src/main/java/com/outthedoor/withsoil/api/ai/controller/AiController.java
@@ -2,9 +2,12 @@ package com.outthedoor.withsoil.api.ai.controller;
 
 import com.outthedoor.withsoil.api.ai.dto.request.AiChatRequestDto;
 import com.outthedoor.withsoil.api.ai.dto.response.AiChatResponseDto;
+import com.outthedoor.withsoil.api.ai.dto.response.AiDiagnosisResponseDto;
 import com.outthedoor.withsoil.api.ai.service.AiService;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api/v1/ai")
@@ -20,6 +23,15 @@ public class AiController {
     public ResponseEntity<AiChatResponseDto> testChat(@RequestBody AiChatRequestDto requestDto) {
 
         AiChatResponseDto responseDto = aiService.chatRag(requestDto.query());
+        return ResponseEntity.ok(responseDto);
+    }
+    @PostMapping(value = "/diagnose", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<AiDiagnosisResponseDto> diagnoseCrop(
+            @RequestParam("crop_name") String cropName,
+            @RequestPart("file") MultipartFile file,
+            @RequestParam(value = "topk", defaultValue = "5") int topk) {
+
+        AiDiagnosisResponseDto responseDto = aiService.diagnoseCrop(cropName, file, topk);
         return ResponseEntity.ok(responseDto);
     }
 }
